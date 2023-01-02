271fa844a93fba95be34e41c33082ee42f6e2f28
Max <grachasaurus@gmail.com>
2023-01-02 09:48:34 +0200

    Add RGB datastruct to Key class. Create a directory for light(n)ing simulator - thor.

--- src/layout/key.py
+++ src/layout/key.py
@@ -1,7 +1,19 @@
 """Class for a key."""
 
-
+from random import randint
 from dataclasses import dataclass
+
+
+@dataclass
+class RGB:
+    r: int
+    g: int
+    b: int
+
+    def randomize(self):
+        self.r = randint(0, 255)
+        self.g = randint(0, 255)
+        self.b = randint(0, 255)
 
 
 @dataclass
@@ -12,4 +24,5 @@ class Key:
     y: int
     width: float = 1.0
     height: float = 1.0
+    rgb: RGB = RGB(255, 255, 255)
     name: str = ""
---
+++ src/thor/__init__.py
@@ -0,0 +1,1 @@
+"""Light(n)ing simulator."""
